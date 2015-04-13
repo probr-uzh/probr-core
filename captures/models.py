e7b2fc086245280d0ff120adced3347d97bba346
Alessandro De Carli <decarli.ale@gmail.com>
2015-04-13 15:04:21 +0200

    - added capture model make sure you do a "brew install geos" (or sudo apt-get install binutils libproj-dev gdal-bin)

--- captures/models.py
+++ captures/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+from taggit.managers import TaggableManager
+from utils.models import BaseModel
+
+
+class Capture(BaseModel):
+    pcap = models.FileField(upload_to="pcap")
+    location = models.PointField()
+
+    tags = TaggableManager()
