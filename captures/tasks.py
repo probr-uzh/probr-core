550286a33135abc0f3213620edcc64aa5fa4c2de
Sebastian Stephan <sstephan1337@gmail.com>
2015-04-20 10:53:19 +0200

    Use parentheses for print to follow python3 standards.

--- captures/tasks.py
+++ captures/tasks.py
@@ -7,4 +7,4 @@ from captures.models import Capture
 @shared_task
 def unpack_capture(captureUUID):
     capture = Capture.objects.get(pk=captureUUID)
-    print capture.pcap
+    print(capture.pcap)
