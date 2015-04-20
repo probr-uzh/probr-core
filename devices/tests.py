d9083d24dfca275a58f07282ba814ea6d1fa0ece
Sebastian Stephan <sstephan1337@gmail.com>
2015-04-20 15:07:10 +0200

    Add unit test for UUID

--- devices/tests.py
+++ devices/tests.py
@@ -1,11 +1,21 @@
 from django.test import TestCase
 from devices.models import Device, Status
+import re
 
 
 class StatusTestCase(TestCase):
 
     def setUp(self):
         self.device = Device.objects.create(name="Pii")
+
+    def test_uuid(self):
+        """
+        :return: Tests uuid to be RFC 4122 conform
+        """
+        status = Status.objects.create(device=self.device)
+        self.assertEqual(len(status.uuid), 36)
+        p = re.compile('^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$', re.IGNORECASE)
+        self.assertTrue(p.match(status.uuid));
 
     def test_usage_calculation(self):
         status = Status.objects.create(device=self.device, total_memory=128, used_memory=64, total_disk=4, used_disk=3)
