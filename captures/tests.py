c60eb949cc5bb247dc243d07abffa417aadf0931
Alessandro De Carli <decarli.ale@gmail.com>
2015-04-27 10:51:55 +0200

    - dpkt unpack test

--- captures/tests.py
+++ captures/tests.py
@@ -1,3 +1,41 @@
+from django.core.files import File
 from django.test import TestCase
 
 # Create your tests here.
+from captures.models import Capture
+import dpkt
+
+class CaptureTestCase(TestCase):
+
+    def setUp(self):
+        self.pcapFile = File(open("testfiles/vm-2.pcap"))
+        self.capture = Capture.objects.create(pcap=self.pcapFile)
+
+    def test_unpacking(self):
+        print self.capture.pcap
+        counter=0
+        ipcounter=0
+        tcpcounter=0
+        udpcounter=0
+
+        pcapReader = dpkt.pcap.Reader(self.capture.pcap)
+        for ts, pkt in pcapReader:
+            print pcapReader.datalink()
+            counter+=1
+            eth=dpkt.ethernet.Ethernet(pkt)
+            if eth.type!=dpkt.ethernet.ETH_TYPE_IP:
+               continue
+
+            ip=eth.data
+            ipcounter+=1
+
+            if ip.p==dpkt.ip.IP_PROTO_TCP:
+               tcpcounter+=1
+
+            if ip.p==dpkt.ip.IP_PROTO_UDP:
+               udpcounter+=1
+
+        print "Total number of packets in the pcap file: ", counter
+        print "Total number of ip packets: ", ipcounter
+        print "Total number of tcp packets: ", tcpcounter
+        print "Total number of udp packets: ", udpcounter
--- devices/tests.py
+++ devices/tests.py
@@ -15,13 +15,13 @@ class StatusTestCase(TestCase):
         status = Status.objects.create(device=self.device)
         self.assertEqual(len(status.uuid), 36)
         p = re.compile('^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$', re.IGNORECASE)
-        self.assertTrue(p.match(status.uuid));
+        self.assertTrue(p.match(status.uuid))
 
     def test_usage_calculation(self):
         status = Status.objects.create(device=self.device, total_memory=128, used_memory=64, total_disk=4, used_disk=3)
-        self.assertEqual(status.total_memory, 128);
-        self.assertEqual(status.used_memory, 64);
-        self.assertEqual(status.total_disk, 4);
-        self.assertEqual(status.used_disk, 3);
+        self.assertEqual(status.total_memory, 128)
+        self.assertEqual(status.used_memory, 64)
+        self.assertEqual(status.total_disk, 4)
+        self.assertEqual(status.used_disk, 3)
         self.assertEqual(status.memory_usage(), 0.5)
         self.assertEqual(status.disk_usage(), 0.75)
