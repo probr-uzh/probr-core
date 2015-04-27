b9c749e3b431c0fc1e066d5af91481de0b33284b
Genc Mazlami <genc.mazlami@uzh.ch>
2015-04-27 22:31:08 +0200

    Fixed Taggit-Saving issue for device REST Endpoint.

--- captures/serializers.py
+++ captures/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework.serializers import ModelSerializer, StringRelatedField
+from taggit_serializer.serializers import TagListSerializerField,TaggitSerializer
 from models import Capture
 
-class CaptureSerializer(ModelSerializer):
+class CaptureSerializer(TaggitSerializer,ModelSerializer):
 
-    tags = StringRelatedField(many=True)
+    tags = TagListSerializerField()
 
     class Meta:
         model = Capture
