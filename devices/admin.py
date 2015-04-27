70fccc8b06c67a6ff9fb3acedec9d40b5d0f1c64
Alessandro De Carli <decarli.ale@gmail.com>
2015-04-27 17:02:38 +0200

    - status added to admin - script with status upload

--- devices/admin.py
+++ devices/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from models import Device
+from models import Device, Status
 
 # Register Device Model to be modifiable by the admin UI
 
 admin.site.register(Device)
+admin.site.register(Status)
 
