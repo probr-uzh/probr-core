5d4d7e36d606b26815766a623f254690c1d5ba51
Joel Scheuner <joel.scheuner.dev@gmail.com>
2015-05-03 19:04:31 +0200

    Cleanup bash script; Add helper functions

--- scripts/device_daemon.sh
+++ scripts/device_daemon.sh
@@ -1,48 +1,58 @@
 #!/usr/bin/env bash
-# Script specific vars
-VERSION="0.1 beta"
+VERSION='0.1.0 beta'
 
+UUID_REGEX='"uuid":"([A-Za-z0-9-]+)"'
+UUID_FILE='uuid.txt'
 
-UUID_REGEX="\"uuid\":\"([A-Za-z0-9-]+)\""
+BASE_URL='http://127.0.0.1:8000'
+CONTENT_TYPE='Content-Type: application/json'
 
-# Api specific vars
-BASE_URL="http://127.0.0.1:8000"
-CONTENT_TYPE="Content-Type: application/json"
-
-
-function registerDevice(){
-    # Device specific vars
-    NAME=$HOSTNAME
-    OS=$(uname -a)
-    TYPE="OWR" #find a way to determine type
-    WIFI="" #find a way to determine wifi_chip
-    DESCRIPTION="added automatically with device script $VERSION"
-
-
-    body_data=\{\"name\":\"197$NAME\",\"os\":\"$OS\",\"type\":\"$TYPE\",\"wifi_chip\":\"$WIFI\",\"description\":\"$DESCRIPTION\"\}
-    response=$(wget -qO- --post-data="$body_data" --header "$CONTENT_TYPE" -- $BASE_URL/api/devices/)
-
-    [[ $response =~ $UUID_REGEX ]]
-    uuid="${BASH_REMATCH[1]}"
-
-    if [ $uuid ]
-    then
-        echo "I managed to register myself, the uuid I received is: $uuid"
-        echo "$uuid">uuid.txt
-    else
-        echo "there was an issue during registration: $response"
-
-    fi
-}
-
-function remoteExecute(){
-    result=$("$1 2>&1")
+execute() {
+    result=$($1 2>&1 </dev/null)
     echo "executed  $1"
     echo "result    $result"
 }
 
-function postStatus(){
-    uuid=$(cat uuid.txt)
+# Issues an http post request to the probr server
+# Arguments:
+#   url
+#   json data
+# Returns:
+#   http response
+post() {
+    echo $(wget --quiet --output-document=- --header "$CONTENT_TYPE" --post-data="$2" -- "$BASE_URL$1")
+}
+
+device_uuid() {
+    echo $(cat "$UUID_FILE")
+}
+
+register_device() {
+    NAME=$HOSTNAME
+    OS=$(uname -a)
+    TYPE='RPB'  # TODO: find a way to determine type
+    WIFI=''     # TODO: find a way to determine wifi_chip
+    DESCRIPTION="Added automatically with device script $VERSION"
+
+    body_data='{"name":"'197$NAME'","os":"'$OS'","type":"'$TYPE'","wifi_chip":"'$WIFI'","description":"'$DESCRIPTION'"}'
+    response=$(post '/api/devices/' "$body_data")
+
+    [[ $response =~ $UUID_REGEX ]]
+    uuid="${BASH_REMATCH[1]}"
+
+    echo "uuid=$uuid"
+
+    if [ $uuid ]
+    then
+        echo "I managed to register myself, the uuid I received is: $uuid"
+        echo "$uuid" > "$UUID_FILE"
+    else
+        echo "there was an issue during registration: $response"
+    fi
+}
+
+post_status() {
+    uuid=$(device_uuid)
 
     total_memory=$(top -bn1 | awk '/KiB Mem/ { print $3 };')
     used_memory=$(top -bn1 | awk '/KiB Mem/ { print $5 };')
@@ -52,9 +62,8 @@ function postStatus(){
 
     cpu_load=$(top -bn1 | awk '/Cpu/ { print 100 - $8};')
 
-    body_data=\{\"device\":\"$uuid\",\"cpu_load\":\"$cpu_load\",\"used_disk\":\"$used_disk\",\"total_disk\":\"$total_disk\",\"used_memory\":\"$used_memory\",\"total_memory\":\"$total_memory\"\}
-
-    response=$(wget -qO- --post-data="$body_data" --header "$CONTENT_TYPE" -- $BASE_URL/api/statuses/)
+    body_data='{"device":"'$uuid'","cpu_load":"'$cpu_load'","used_disk":"'$used_disk'","total_disk":"'$total_disk'","used_memory":"'$used_memory'","total_memory":"'$total_memory'"}'
+    response=$(post '/api/statuses/' "$body_data")
 
     [[ $response =~ $UUID_REGEX ]]
     uuid="${BASH_REMATCH[1]}"
@@ -67,23 +76,23 @@ function postStatus(){
     fi
 }
 
-function main(){
-    if [ -a uuid.txt ]
+main() {
+    execute 'echo "Hello World!"'
+
+    if [ -a "$UUID_FILE" ]
     then
-        uuid=$(cat uuid.txt)
-        echo "you are using: $uuid"
+        echo "you are using: $(device_uuid)"
     else
         echo "first time execution, registering device"
-        registerDevice
+        register_device
     fi
 
     while :
     do
         echo "infinite loops [ hit CTRL+C to stop]"
-        postStatus &
+        post_status &
         sleep 5
     done
 }
 
-remoteExecute 'echo he'
-main
+main "$@"
