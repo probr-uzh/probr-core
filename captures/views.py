45875f210a9d9ed24fb61cf762ebae9f78851b8a
Genc Mazlami <genc.mazlami@uzh.ch>
2015-05-11 20:40:23 +0200

    Implement trigger for async capture task.

--- captures/views.py
+++ captures/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import generics
+from tasks import unpack_capture
 from rest_framework.renderers import JSONRenderer
 from serializers import CaptureSerializer
 from rest_framework.parsers import MultiPartParser, FormParser
@@ -21,13 +22,13 @@ class CaptureUploadView(generics.ListCreateAPIView):
 
     def post(self, request, *args, **kwargs):
         instance = Capture(pcap=request.FILES['pcap'],longitude=request.DATA['longitude'],latitude=request.DATA['latitude'])
-        print("Tags: " + request.DATA['tags'])
         instance.save()
         tags = request.DATA['tags']
         tag_list = tags.split(",")
         for tag in tag_list:
             instance.tags.add(tag)
         instance.save()
+        unpack_capture.delay(instance.pk)
         return Response('Capture upload successful')
 
 
