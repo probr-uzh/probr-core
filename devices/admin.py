e421a8de42c536c04df3c8f7ff65177c47c6cb8d
Sebastian Stephan <sstephan1337@gmail.com>
2015-06-08 15:36:54 +0200

    Make the admin view for 'Command' more readable and sort by most recent modied at top.

--- devices/admin.py
+++ devices/admin.py
@@ -17,9 +17,17 @@ class StatusAdmin(admin.ModelAdmin):
     creation_time_detailed.short_description = "Posted at"
 
     list_display = ('creation_time_detailed', 'device', 'ip')
-
     ordering = ['-creation_timestamp']
 
-admin.site.register(Command)
+@admin.register(Command)
+class CommandAdmin(admin.ModelAdmin):
+
+    def modification_time_detailed(self, obj):
+        return obj.modification_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+    modification_time_detailed.admin_order_field = 'modification_timestamp'
+    modification_time_detailed.short_description = "Last modified at"
+
+    list_display = ('modification_time_detailed', 'execute', 'device', 'status')
+    ordering = ['-modification_timestamp']
 
 
