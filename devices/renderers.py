51cd21282e0323d7f3e4dd91957019f976389956
Sebastian Stephan <sstephan1337@gmail.com>
2015-06-08 15:06:43 +0200

    A single 'Command' renders to an actual script (with shebang firstline)

--- devices/renderers.py
+++ devices/renderers.py
@@ -2,13 +2,19 @@ __author__ = 'ale'
 from django.utils.encoding import smart_unicode
 from rest_framework import renderers
 
+
 class PlainTextCommandRenderer(renderers.BaseRenderer):
+    """
+    Renders a single command object to a shell script
+    """
+    shebang = '#!/usr/bin/env sh'
     media_type = 'text/plain'
     format = 'txt'
 
     def render(self, data, media_type=None, renderer_context=None):
         if isinstance(data, dict) and data.has_key('execute'):
-            return data['execute']
+            output = "%s\n%s\n" % (self.shebang, data['execute'])
+            return output
         else:
             return data
 
