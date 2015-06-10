8cf13b7c2143258551e5d7bc4756711a80c4ebf8
Sebastian Stephan <sstephan1337@gmail.com>
2015-06-10 22:56:09 +0200

    Version 0.2 of daemon script. Remote execution improved.

--- scripts/device_daemon.sh
+++ scripts/device_daemon.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-VERSION='0.1.1 beta'
-# DEPENDENCIES
-# ./JSON.sh from https://github.com/dominictarr/JSON.sh must be executable
+VERSION='0.2'
+BASE_HOST='probr.sook.ch'
+BASE_URL="https://$BASE_HOST"
+UUID_FILE='uuid.txt'
 
 # Make the script working location independent
 BASEDIR=$(dirname $0)
@@ -20,20 +21,6 @@ clean_up() {
 }
 trap 'clean_up' EXIT
 
-
-BASE_HOST='probr.sook.ch'
-BASE_URL="https://$BASE_HOST"
-
-CONTENT_TYPE='Content-Type: application/json'
-UUID_FILE='uuid.txt'
-
-install_dependencies() {
-    if [ ! -f JSON.sh ]; then
-        wget 'https://raw.githubusercontent.com/dominictarr/JSON.sh/master/JSON.sh'
-        chmod +x JSON.sh
-    fi
-}
-
 # Performs regular expression matching using extended regex (i.e., no escaping required)
 # Arguments:
 #   string
@@ -41,13 +28,7 @@ install_dependencies() {
 # Example:
 #   extract '"id":"example_id"' '"id":"([a-zA-Z-_]+)"'
 extract() {
-	echo $1 | sed -r 's/^.*'$2'.*$/\1/'
-}
-
-# Execute an arbitrary bash command and return stdout and stderr
-execute() {
-    result=$($1 2>&1 </dev/null)
-    echo $result
+	echo $1 | sed -E 's/^.*'$2'.*$/\1/'
 }
 
 # Issues an http post request with json payload to the probr server
@@ -57,24 +38,17 @@ execute() {
 # Returns:
 #   http response
 post() {
-#    WGETCHECK=$(wget --help|grep -o post-data)
-#    if [ -n "$WGETCHECK" ]; then
-#        local CONTENT_LEN=$(echo  $2 | wc -c)
-#        local raw_payload="POST $1 HTTP/1.0\r\nHost: $BASE_HOST\r\n$CONTENT_TYPE\r\nContent-Length: $CONTENT_LEN\r\n\r\n$2"
-#        echo $( (echo $raw_payload && sleep 5) | openssl s_client -connect $BASE_HOST:443 2>&1 )
-#    else
-
-        echo $(wget --no-check-certificate --quiet --output-document=- --header "$CONTENT_TYPE" --post-data="$2" -- "$BASE_URL$1")
-#    fi
+    echo $(wget --quiet --output-document=- --header 'Content-Type: application/json' --post-data="$2" -- "$BASE_URL$1")
 }
 
 # Issues an http get request to the probr server
 # Arguments:
 #   url
+#   header (e.g. 'Accept: text/plain')
 # Returns:
 #   http response
 get() {
-    echo $(wget --no-check-certificate --quiet --output-document=- -- "$BASE_URL$1")
+    echo $(wget --quiet --header "$2" --output-document=- -- "$BASE_URL$1")
 }
 
 # Read the device uuid from file
@@ -92,10 +66,10 @@ extract_uuid() {
 }
 
 register_device() {
-    NAME=$HOSTNAME # TODO: hostname is very unlikely a unique device name
-    OS=$(uname -a)
-    TYPE='RPB'  # TODO: find a way to determine device type
-    WIFI=''     # TODO: find a way to determine wifi_chip
+    NAME=$(uname -n)    # TODO: hostname is very unlikely a unique device name
+    OS=$(uname -a)      # All informations from uname
+    TYPE='RPB'          # odroid: armv71, openwrt(vbox): i686, pc/laptop 64 bit: x86_64
+    WIFI=''             # TODO: find a way to determine wifi_chip
     DESCRIPTION="Added automatically with device script $VERSION"
 
     body_data='{"name":"'$NAME'","os":"'$OS'","tags":[],"type":"'$TYPE'","wifi_chip":"'$WIFI'","description":"'$DESCRIPTION'"}'
@@ -121,13 +95,17 @@ bootstrap_device() {
 }
 
 post_status() {
-    total_memory=$(top -bn1 | awk '/KiB Mem/ { print $3 };')
-    used_memory=$(top -bn1 | awk '/KiB Mem/ { print $5 };')
+    #total_memory=$(top -bn1 | awk '/KiB Mem/ { print $3 };')
+    #used_memory=$(top -bn1 | awk '/KiB Mem/ { print $5 };')
+    #total_disk=$(df -k . | awk 'NR==2 { print $4 };')
+    #used_disk=$(df -k . | awk 'NR==2 { print $3 };')
+    #cpu_load=$(top -bn2 | awk '/Cpu/ { print 100 - $8};' | tail -n1)
 
-    total_disk=$(df -k . | awk 'NR==2 { print $4 };')
-    used_disk=$(df -k . | awk 'NR==2 { print $3 };')
-
-    cpu_load=$(top -bn2 | awk '/Cpu/ { print 100 - $8};' | tail -n1)
+    total_memory=100
+    used_memory=10
+    total_disk=1000
+    used_disk=100
+    cpu_load=0.1
 
     body_data='{"device":"'$(device_uuid)'","cpu_load":"'$cpu_load'","used_disk":"'$used_disk'","total_disk":"'$total_disk'","used_memory":"'$used_memory'","total_memory":"'$total_memory'"}'
     response=$(post '/api/statuses/' "$body_data")
@@ -173,35 +151,71 @@ get_commands() {
 }
 
 # Arguments:
-#   result
-#   command uuid
+#   logFilePath
+#   commanduuid
 submit_result() {
-    body_data='{"result":"'$1'"}'
-    response=$(post "/api/commands/$2/" "$body_data")
+    # We need to make a copy of the log file, since curl can't handle the still opened log file
+    cp "$1" "$1.upload"
+    echo $(curl -v -F result=@$1.upload $BASE_URL/api/commands/"$2"/)
+    rm "$1.upload"
 }
 
-# TODO: Support multi-command execution
-# TODO: Refactor this (e.g., query get_commands only once)
-# TODO: Properly handle result escaping (maybe via file upload) !
+# Arguments:
+#   commanduuid
+#   status (1 = started, 2 = executed)
+set_command_status() {
+    echo $(post "/api/commands/$1/" "{\"status\":$2}")
+}
+
+# Arguments
+#   commanduuid
+cleanup_command() {
+    submit_result "commands/$1.log" $1
+    rm commands/$1*
+    exit
+}
+
+# Arguments:
+#   scriptPath
+#   logFilePath
+#   commandUUID
+execute_in_background() {
+    (trap "cleanup_command $3" EXIT; source $1) 1>$2 2>&1 &
+    echo $!>"commands/$3.pid"
+    echo $!
+}
+
 execute_commands() {
-    commands=$(get_commands)
-    first_command=$(parse_json "$commands" "[\"results\",0,\"execute\"]")
-    first_uuid=$(parse_json "$commands" "[\"results\",0,\"uuid\"]")
-    if [ $first_command ]; then
-        result=$(execute "$first_command")
-        submit_result "$result" "$first_uuid"
-        echo "Successfully executed \"$first_command\" and submitted result: $result"
-    fi
+    mkdir -p commands
+
+    # Get list of all not exucted commands
+    commands=$(get "/api/commands/?device=$(device_uuid)&status=0" 'Accept: text/plain')
+
+    # Download the command scripts to ./commands folder in the format UUID.sh
+    # and make them executable
+    for commanduuid in $commands; do
+        echo "Downloading command: $commanduuid"
+        wget --quiet --header 'Accept: text/plain' --output-document="./commands/$commanduuid.sh" -- "$BASE_URL/api/commands/$commanduuid"
+        chmod +x "./commands/$commanduuid.sh"
+
+        # Set status of command to 'Executing' (1)
+        result=$(set_command_status "$commanduuid" 1)
+
+        # Execute script, while piping all output 
+        pid=$(execute_in_background "./commands/$commanduuid.sh" "commands/$commanduuid.log" "$commanduuid")
+
+        echo "Started command $commanduuid with pid $pid"
+
+    done
 }
 
 main() {
-    install_dependencies
     bootstrap_device
 
     while :
     do
-        echo "Infinite loop [ hit CTRL+C to stop]"
-        post_status
+        echo "Infinite loop [hit CTRL+C to stop]"
+        #post_status
         execute_commands
         sleep 5
     done
