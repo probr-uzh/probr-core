131ed9215e7024ae62c3cee9452a9f015f308ee7
Dominik Schöni <dominik@dominikschoeni.ch>
2015-06-15 12:13:09 +0200

    capture do live-reload

--- frontend/app/captures/captures.controller.js
+++ frontend/app/captures/captures.controller.js
@@ -4,8 +4,7 @@ angular.module('probrApp')
     .controller('CapturesCtrl', function ($scope, Capture, resourceSocket) {
         Capture.query({limit: 50}, function (resultObj) {
             $scope.captures = resultObj.results;
-            $scope.numberOfCaptures = resultObj.count;
-            resourceSocket.updateResource($scope, $scope.captures, 'capture');
+            resourceSocket.updateResource($scope, $scope.captures, 'capture', 'uuid', 50);
         });
     });
 ;
