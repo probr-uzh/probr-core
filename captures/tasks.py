29ba78f897b73d4ff62b1d87f1c9a679a254636d
Alessandro De Carli <decarli.ale@gmail.com>
2015-06-22 18:09:10 +0200

    - new resolver

--- captures/tasks.py
+++ captures/tasks.py
@@ -4,12 +4,18 @@ from celery import shared_task
 from captures.models import Capture
 from probr.base_settings import PROBR_HANDLERS
 
-def recursiveImport(name):
-    mod = __import__(name)
-    components = name.split('.')
-    for comp in components[1:]:
-        mod = getattr(mod, comp)
-    return mod
+import importlib
+
+def recursiveImport(full_class_string):
+    """
+    dynamically load a class from a string
+    """
+    class_data = full_class_string.split(".")
+    module_path = ".".join(class_data[:-1])
+    class_str = class_data[-1]
+    module = importlib.import_module(module_path)
+    # Finally, we retrieve the Class
+    return getattr(module, class_str)
 
 @shared_task
 def processCapture(captureUUID):
