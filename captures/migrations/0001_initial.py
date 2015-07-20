0e05feda65862e9e9a22f4742fa018eb942633ee
Sebastian Stephan <sstephan1337@gmail.com>
2015-07-20 23:23:59 +0200

    Reset all Migrations to start with new empty Database (will lose all data)

--- captures/migrations/0001_initial.py
+++ captures/migrations/0001_initial.py
@@ -2,30 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import taggit.managers
-import uuid
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('taggit', '0001_initial'),
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Capture',
-            fields=[
-                ('uuid', models.UUIDField(default=uuid.uuid4, serialize=False, verbose_name=b'ID', primary_key=True)),
-                ('creation_timestamp', models.DateTimeField(auto_now_add=True, db_index=True)),
-                ('modification_timestamp', models.DateTimeField(auto_now=True, db_index=True)),
-                ('pcap', models.FileField(upload_to=b'pcap')),
-                ('longitude', models.FloatField(default=0)),
-                ('latitude', models.FloatField(default=0)),
-                ('tags', taggit.managers.TaggableManager(to='taggit.Tag', through='taggit.TaggedItem', help_text='A comma-separated list of tags.', verbose_name='Tags')),
-            ],
-            options={
-                'abstract': False,
-            },
-        ),
     ]
