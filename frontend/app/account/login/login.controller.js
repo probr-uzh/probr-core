cc233bb4bef91969bc351b86c877cb08e79e1c61
Dominik Schöni <dominik@dominikschoeni.ch>
2015-07-29 13:59:07 +0200

    testing fix for #1355

--- frontend/app/account/login/login.controller.js
+++ frontend/app/account/login/login.controller.js
@@ -1,26 +1,34 @@
 'use strict';
 
 angular.module('probrApp')
-  .controller('LoginCtrl', function ($scope, Auth, $location) {
-    $scope.user = {};
-    $scope.errors = {};
+    .controller('LoginCtrl', function ($scope, Auth, $location) {
 
-    $scope.login = function(form) {
-      $scope.submitted = true;
-
-      if(form.$valid) {
-        Auth.login({
-          username: $scope.user.username,
-          password: $scope.user.password
-        })
-        .then( function() {
-          // Logged in, redirect to home
-          $location.path('/devices');
-        })
-        .catch( function(err) {
-          $scope.errors.other = err.message;
+        Auth.isLoggedInAsync(function (isLoggedIn) {
+            if (isLoggedIn) {
+                $location.path('/devices');
+                return;
+            }
         });
-      }
-    };
 
-  });
+        $scope.user = {};
+        $scope.errors = {};
+
+        $scope.login = function (form) {
+            $scope.submitted = true;
+
+            if (form.$valid) {
+                Auth.login({
+                    username: $scope.user.username,
+                    password: $scope.user.password
+                })
+                    .then(function () {
+                        // Logged in, redirect to home
+                        $location.path('/devices');
+                    })
+                    .catch(function (err) {
+                        $scope.errors.other = err.message;
+                    });
+            }
+        };
+
+    });
