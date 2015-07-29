455bc473e179b34def2d1218230f5ce6ccadd9f0
Joel Scheuner <joel.scheuner.dev@gmail.com>
2015-07-29 17:02:29 +0200

    Move flushdata and create admin user to from run to setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+flush_data=false
+
+if [ "$flush_data" = true ]; then
+	python manage.py flush --noinput
+	echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
+fi
+
 pip install -r requirements.txt
 python manage.py migrate
 bower install
