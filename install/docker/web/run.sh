d0dece534f9c450a8e10fcb7e4ae3cdee403519d
Dominik Schöni <dominik@dominikschoeni.ch>
2015-10-05 17:39:15 +0200

    fixing performance-issues with uwsgi due to misconfiguration

--- install/docker/web/run.sh
+++ install/docker/web/run.sh
@@ -2,4 +2,4 @@
 
 python manage.py collectstatic --noinput
 python manage.py migrate
-uwsgi --socket :8001 --module probr.wsgi_django:application --enable-threads
+uwsgi --socket :8001 --module probr.wsgi_django:application --processes 4 --master --enable-threads
