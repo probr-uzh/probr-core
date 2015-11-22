ab0ce577489821d6f8b7147d9ad9cb8e7f330e67
Dominik Schöni <dominik@dominikschoeni.ch>
2015-11-22 15:16:02 +0100

    fixing problems with registering multiple callbacks on same socket-resource

--- frontend/app/resources/resource.socket.js
+++ frontend/app/resources/resource.socket.js
@@ -8,21 +8,21 @@ angular.module('probrApp')
 
         this.updateResource = function ($scope, resource, objectName, bufferSize, updateAndCreate, uuidFilter, uuid) {
 
-            resource.uuidFilter = {};
-            resource.uuidFilter.filter = uuidFilter || 'uuid';
-            resource.uuidFilter.uuid = uuid;
-
-            resource.objectName = objectName;
-            resource.bufferSize = bufferSize || 0;
-            resource.updateAndCreate = updateAndCreate || false;
-
             // remove resource object when its scope gets destroyed
             $scope.$on('$destroy', function () {
-                so.watchedResources.splice(so.watchedResources.indexOf(resource), 1);
+                so.watchedResources.splice(_.filter(so.watchedResources, {resource: resource}), 1);
             });
 
             // watch resource
-            so.watchedResources.push(resource);
+            so.watchedResources.push({
+                resource: resource,
+                properties: {
+                    uuidFilter: { filter: uuidFilter || 'uuid', uuid: uuid },
+                    objectName: objectName,
+                    bufferSize: bufferSize || 0,
+                    updateAndCreate: updateAndCreate || false
+                }
+            });
         }
 
         this.disconnect = function () {
@@ -53,23 +53,20 @@ angular.module('probrApp')
             so.socket.onMessage(function (message) {
 
 
-
                 var dataObj = JSON.parse(message.data);
                 dataObj.timestamp = message.timeStamp;
 
                 var resources = [];
                 _.forEach(so.watchedResources, function (resourceObj) {
-                    if ((resourceObj.objectName + ':update') === dataObj.object_type) {
+                    if ((resourceObj.properties.objectName + ':update') === dataObj.object_type) {
 
                         // check if we have a custom query in place
-                        if (resourceObj.uuidFilter.filter !== 'uuid') {
+                        if (resourceObj.properties.uuidFilter.filter !== 'uuid') {
 
-                            if (dataObj.hasOwnProperty([resourceObj.uuidFilter.filter]) && dataObj[resourceObj.uuidFilter.filter] === resourceObj.uuidFilter.uuid) {
-
+                            if (dataObj.hasOwnProperty([resourceObj.properties.uuidFilter.filter]) && dataObj[resourceObj.properties.uuidFilter.filter] === resourceObj.properties.uuidFilter.uuid) {
                                 resources.push(resourceObj);
-                                //console.log(resources.length);
-
                             }
+
                         } else {
                             resources.push(resourceObj);
                         }
@@ -80,22 +77,18 @@ angular.module('probrApp')
                 $rootScope.$apply(function () {
                     _.forEach(resources, function (resourceObj) {
 
-                        // define filterpredict to look for uuid or foreign-key that is a uuid
-                        var filterPredicate = {};
-                        filterPredicate.uuid = dataObj.uuid;
-
                         // look for objects that can be updated
-                        var updateableObjects = _.filter(resourceObj, filterPredicate);
+                        var updateableObjects = _.filter(resourceObj.resource,  {uuid: dataObj.uuid});
 
                         if (updateableObjects.length > 0) {
                             _.forEach(updateableObjects, function (obj) {
                                 _.merge(obj, dataObj);
                             });
                         } else {
-                            if (resourceObj.updateAndCreate) {
-                                resourceObj.push(dataObj);
-                                if (resourceObj.bufferSize > 0 && resourceObj.length > resourceObj.bufferSize) {
-                                    resourceObj.shift();
+                            if (resourceObj.properties.updateAndCreate) {
+                                resourceObj.resource.push(dataObj);
+                                if (resourceObj.properties.bufferSize > 0 && resourceObj.resource.length > resourceObj.properties.bufferSize) {
+                                    resourceObj.resource.shift();
                                 }
                             }
                         }
