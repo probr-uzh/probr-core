45cd56aca0ecbd9eebe0d24145bbba2aa64039cc
Alessandro De Carli <decarli.ale@gmail.com>
2015-12-14 15:26:21 +0100

    - sequence number now gets read correctly

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,41 +1,47 @@
 import binascii
-import json
 import datetime
+import json
+
 import dpkt
 from bson import json_util
-from captures.models import Capture
-from utils.models import publishMessage
-from probr import mongodb
-from probr import socketioemitter
-from probr.base_settings import WS4REDIS_CONNECTION
-from probr import influx_config
 from influxdb import InfluxDBClient
 from influxdb.exceptions import InfluxDBClientError
 
+from probr import influx_config
+from probr import mongodb
+from probr import socketioemitter
+from utils.models import publishMessage
 
 __author__ = 'ale'
 
+
+# sequence control is 2 byte long, the first 12 bits belong to the sequence number, the remaining 4 bits belong to the fragment number
+def parseSequenceControl(s):
+    result = ""
+    for c in reversed(s):
+        bits = bin(ord(c))[2:]
+        bits = '00000000'[len(bits):] + bits
+        result += bits
+    return int(result[:12], 2), int(result[12:16], 2)
+
+
 def generate_json(capture, packet, timestamp):
     tap = dpkt.radiotap.Radiotap(packet)
-    t_len = binascii.hexlify(packet[2:3])    #t_len field indicates the entire length of the radiotap data, including the radiotap header.
-    t_len = int(t_len,16)
-
-    wlan = dpkt.ieee80211.IEEE80211(packet[t_len:])
-
-    # todo: this can be extended to all necessary fields / data we need, done even on demand
+    t_len = binascii.hexlify(packet[2:3])  # t_len field indicates the entire length of the radiotap data, including the radiotap header.
+    t_len = int(t_len, 16)
+    ieee80211Frame = packet[t_len:]
+    sequenceControl = packet[t_len + 22:t_len + 24]
+    wlan = dpkt.ieee80211.IEEE80211(ieee80211Frame)
     jsonPacket = {}
     jsonPacket['capture_uuid'] = capture.uuid
-
+    jsonPacket['sequence_number'], jsonPacket['fragment_number'] = parseSequenceControl(sequenceControl)
     if len(capture.tags.all()) > 0:
-       jsonPacket['tags'] = list(capture.tags.names())
-
+        jsonPacket['tags'] = list(capture.tags.names())
     jsonPacket['inserted_at'] = datetime.datetime.utcnow()
-    jsonPacket['location'] = { 'type': 'Point', 'coordinates': [capture.longitude, capture.latitude] }
-
+    jsonPacket['location'] = {'type': 'Point', 'coordinates': [capture.longitude, capture.latitude]}
     jsonPacket['time'] = datetime.datetime.utcfromtimestamp(timestamp)
-
     try:
-        jsonPacket['signal_strength'] = -(256-tap.ant_sig.db)
+        jsonPacket['signal_strength'] = -(256 - tap.ant_sig.db)
     except:
         pass
     try:
@@ -50,13 +56,8 @@ def generate_json(capture, packet, timestamp):
         jsonPacket['mac_address_dst'] = binascii.hexlify(wlan.mgmt.dst)
     except:
         pass
-    try:
-        jsonPacket['squence_number'] = wlan.tcp.seq
-    except:
-        pass
-
-
     return jsonPacket
+
 
 class MongoDBHandler(object):
     def handle(self, capture):
@@ -71,6 +72,7 @@ class MongoDBHandler(object):
 
                 packets.insert_one(jsonPacket)
 
+
 class WebsocketHandler(object):
     def handle(self, capture):
         if capture.file.size > 0:
@@ -84,6 +86,7 @@ class WebsocketHandler(object):
                 jsonPacket["object_type"] = "packet:update"
                 publishMessage("socket", message=json.dumps(jsonPacket, default=json_util.default))
 
+
 class SocketIOHandler(object):
     def handle(self, capture):
         if capture.file.size > 0:
@@ -94,15 +97,16 @@ class SocketIOHandler(object):
                 jsonPacket = generate_json(capture, packet, timestamp)
 
                 # broadcast to socket
-                socketioemitter.io.Emit("packet:" + jsonPacket['mac_address_src'], json.dumps(jsonPacket, default=json_util.default))
+                socketioemitter.io.Emit("packet:" + jsonPacket['mac_address_src'],
+                                        json.dumps(jsonPacket, default=json_util.default))
                 socketioemitter.io.Emit("packet:create", json.dumps(jsonPacket, default=json_util.default))
 
+
 class InfluxDBHandler(object):
-
     def __init__(self):
-        self.client = InfluxDBClient(influx_config.influx_host, influx_config.influx_port, influx_config.influx_user, influx_config.influx_pw, influx_config.influx_db)
+        self.client = InfluxDBClient(influx_config.influx_host, influx_config.influx_port, influx_config.influx_user,
+                                     influx_config.influx_pw, influx_config.influx_db)
         self.initDb(self.client)
-
 
     def handle(self, capture):
         if capture.file.size > 0:
@@ -114,18 +118,15 @@ class InfluxDBHandler(object):
                 points = self.restructure(jsonPacket)
                 self.client.write_points(points)
 
-
-
-    #check if db exists, if not create it, else do nothing
-    def initDb(self,client):
+    # check if db exists, if not create it, else do nothing
+    def initDb(self, client):
         try:
             client.create_database(influx_config.influx_db)
         except InfluxDBClientError:
             return
 
-
-    #We have to rewrite the packet structure since
-    #for influxdb, the tags and fields in a packet must be in the form of:
+    # We have to rewrite the packet structure since
+    # for influxdb, the tags and fields in a packet must be in the form of:
 
     # packet = {
     #    tags : {
@@ -152,7 +153,7 @@ class InfluxDBHandler(object):
         tags = jsonPacket["tags"]
         influx_tags = {}
         for tag in tags:
-            influx_tags[tag]=tag
+            influx_tags[tag] = tag
 
         # put tags in new packet
         new_packet["tags"] = influx_tags
